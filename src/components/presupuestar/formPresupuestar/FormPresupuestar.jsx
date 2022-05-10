5fe3e6f6cad011906e43a242f332583856af049b
Ezequiel <ezequielamin@outlook.com>
2022-05-10 17:12:32 -0300

    edited swal and added padding

--- src/components/presupuestar/formPresupuestar/FormPresupuestar.jsx
+++ src/components/presupuestar/formPresupuestar/FormPresupuestar.jsx
@@ -67,7 +67,7 @@ const FormPresupuestar = (props) => {
         confirmButtonColor: "#77dd77",
         cancelButtonColor: "#8d8d8d",
         confirmButtonText: "Contactar",
-        footer: `<p className="mb-0">Recomiento leer los <a href="https://bit.ly/presupuestoDJ">terminos y condiciones</a></p>`,
+        footer: `<p className="mb-0">Recomiento leer los <a href="https://bit.ly/pre300622">terminos y condiciones</a></p>`,
       }).then((result) => {
         if (result.isConfirmed) {
           const text = `Hola Ezequiel, quiero presupuestar la siguiente fiesta:%0A
