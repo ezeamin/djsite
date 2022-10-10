ed1a29af71ae92c3400256609802e4546e0ae162
Ezequiel Amin <ezequielamin@outlook.com>
2022-10-10 20:21:55 -0300

    modifico mensaje de despedida

--- src/components/goodbye/GoodbyeScreen.jsx
+++ src/components/goodbye/GoodbyeScreen.jsx
@@ -10,7 +10,7 @@ const GoodbyeScreen = () => {
       </div>
       <Title text="Gracias" />
       <p className='text-light fw-bold'>Ha sido mucho el tiempo que nos hicimos felices, noche a noche, pero llegó mi hora de despedirme de ustedes. Agradezco a todos los que formaron parte de este hermoso proyecto que duró poco más de 5 años, sumando más de 150 eventos unicos.</p>
-      <p className='text-light fw-bold'>A partir del <span className='text-danger'>01/01/2023</span>, no tomaré más fechas.</p>
+      <p className='text-light fw-bold'>A partir del <span className='text-danger'>01/01/2023</span>, no tomaré más fechas. Una etapa que termina, una nueva que comienza...</p>
       <p className='text-light text-end fw-bold'>Hasta siempre, Eze <i className='fa-solid fa-heart text-danger'></i></p>
     </div>
   )
