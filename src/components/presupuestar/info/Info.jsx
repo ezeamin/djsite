4a4218801292c506e560ba429317abb75feb360d
Ezequiel Amin <ezequielamin@outlook.com>
2022-10-19 23:08:20 -0300

    info msg changed

--- src/components/presupuestar/info/Info.jsx
+++ src/components/presupuestar/info/Info.jsx
@@ -4,7 +4,7 @@ import { Alert } from 'react-bootstrap';
 const Info = () => {
     return (
         <div>
-            <Alert severity="info"><b>Atencion:</b> El DJ no tomará fechas los dias viernes (excepto bares)</Alert>
+            <Alert severity="info"><b>Atencion:</b> El DJ no tomará fechas los dias viernes (excepto bares) hasta el 11/11 inclusive</Alert>
         </div>
     );
 };
