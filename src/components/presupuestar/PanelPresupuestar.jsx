9c3ff50c8966ae6e0dd0ce362a31b9fe0ecf9448
Ezequiel Amin <ezequielamin@outlook.com>
2022-11-25 07:06:40 -0300

    advertencia

--- src/components/presupuestar/PanelPresupuestar.jsx
+++ src/components/presupuestar/PanelPresupuestar.jsx
@@ -1,25 +1,33 @@
-import React from "react";
-// import Swal from "sweetalert2";
-import BackButton from "../backButton/BackButton";
-import Title from "../title/Title";
-import FormPresupuestar from "./formPresupuestar/FormPresupuestar";
-import Info from "./info/Info";
-import "./presupuestar.css";
-import Price from "./price/Price";
+import React, { useEffect } from 'react';
+import Swal from 'sweetalert2';
+import BackButton from '../backButton/BackButton';
+import Title from '../title/Title';
+import FormPresupuestar from './formPresupuestar/FormPresupuestar';
+// import Info from "./info/Info";
+import './presupuestar.css';
+import Price from './price/Price';
 
 const PanelPresupuestar = () => {
   const [estMay, setEstMay] = React.useState(false);
   const [estMen, setEstMen] = React.useState(false);
 
-  const [price, setPrice] = React.useState("0");
+  const [price, setPrice] = React.useState('0');
+
+  useEffect(() => {
+    Swal.fire({
+      title: 'Atencion',
+      text: 'Servicio temporalmente no disponible. Por favor, contactar con Ezequiel para presupuestar',
+      icon: 'warning',
+    });
+  }, []);
 
   return (
-    <div className="container">
-      <div className="presupuestar__header mb-2">
-        <BackButton className="my-2" />
+    <div className='container'>
+      <div className='presupuestar__header mb-2'>
+        <BackButton className='my-2' />
         <Price price={price} estMay={estMay} estMen={estMen} />
       </div>
-      <Title text="Presupuestar" />
+      <Title text='Presupuestar' />
       <FormPresupuestar
         setEstMay={setEstMay}
         setEstMen={setEstMen}
